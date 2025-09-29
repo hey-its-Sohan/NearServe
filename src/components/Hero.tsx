04ca4b0edffd9a052b01275985993392d8d77b76
hey-its-Sohan <its.sohan.dev@gmail.com>
2025-09-29 18:23:31 +0600

    first commit

---
+++ src/components/Hero.tsx
@@ -0,0 +1,15 @@
+
+
+const Hero = () => {
+  return (
+    <section className="py-20 lg:py-32">
+      <div  className="mx-auto text-center">
+       <h1 className="text-secondary text-5xl font-bold">
+  Find Local Services Near You
+</h1>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
